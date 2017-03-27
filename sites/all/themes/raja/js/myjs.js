2478a97718beb0c6f457af5def483803eaacd02c
Rahaf <rahaf@cloudappers.com>
2017-03-27 08:49:29 +0200

    refix accessories page

--- sites/all/themes/raja/js/myjs.js
+++ sites/all/themes/raja/js/myjs.js
@@ -242,6 +242,6 @@ jQuery(document).ready(function() {
     if(jQuery('#block-block-8').length > 0) {
         jQuery('#block-block-8').insertAfter('#edit-field-screen-size-value-wrapper')
         jQuery('form#views-exposed-form-mobile-grid-block .views-exposed-widgets > div:not(:first-child), #block-block-8').wrapAll('<div class="panel panel-default"><div id="collapse1" class="panel-collapse collapse"></div></div>')
-        jQuery('<div class="panel-heading"><h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion" href="#collapse1">Collapsible Group 1</a></h4></div>').insertAbefore('#collapse1');
+        jQuery('<div class="panel-heading"><h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion" href="#collapse1">'+ Drupal.t('Advanced Search')+' </a></h4></div>').insertBefore('#collapse1');
     }
 });
