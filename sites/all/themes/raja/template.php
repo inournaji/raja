87f1cf9481d740f742821501970e2d3e2e7ca2f8
Rahaf <rahaf@cloudappers.com>
2017-05-01 23:38:49 +0300

    add devel and hook for biews.

--- sites/all/themes/raja/template.php
+++ sites/all/themes/raja/template.php
@@ -5,23 +5,7 @@
  */
 
 
-function raja_views_pre_view(&$view) {
-    if ($view->name == 'warranty_api' && $view->current_display == 'services_2') {
 
-
-
-
-            $filters = $view->display_handler->get_option('filters');
-            $_SESSION['filters'] = $filters;
-//            foreach ($exposed_filters as $key => $valor) {
-//                $filters[$key]['value'] = $valor;
-//                $view->display_handler->set_option('filters', $filters);
-//                $view->display_handler->override_option('filters', $filters);
-//            }
-
-
-    }
-}
 
 
 
